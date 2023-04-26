71a78cd58a91184225f5438e62b32a233dfc8051
alexandrogf <alexandro.software@gmail.com>
2023-04-26 15:45:48 -0300

    Criado a Classe Department

---
+++ SalesWebMVC/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿
+namespace SalesWebMVC.Models
+{
+    public class Department
+    {
+        // PROPRIEDADES
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // METODOS
+
+    }
+}
